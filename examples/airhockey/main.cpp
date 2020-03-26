d32321b0bd3a8554e05a66bcf4c5b50c3a4c5267
Stijn Mathysen <stijn@skylight.be>
2020-03-26 18:55:19 +0100

    start building air hockey game

--- examples/airhockey/main.cpp
+++ examples/airhockey/main.cpp
@@ -2,45 +2,76 @@
 
 #include <SL_GameEngine.h>
 
+class Puck : public SL_GameObject
+{
+};
+
+class Border : public SL_GameObject
+{
+};
+
 class MyGameEngine : public SL_GameEngine
 {
-	void afterSetup()
+	Puck *puck;
+	Border *topBorder, *leftBorder, *rightBorder, *bottomBorder;
+
+	void setupBorders()
 	{
-		printf("after setup called!\n");
+		topBorder = new Border();
+		topBorder->setup("assets/border.png", getRenderer(), getWidth() / 2.0, 8, getWidth(), 16);
+		addGameObject(topBorder);
+		leftBorder = new Border();
+		leftBorder->setup("assets/border.png", getRenderer(), 8, getHeight() / 2.0, 16, getHeight());
+		addGameObject(leftBorder);
+		bottomBorder = new Border();
+		bottomBorder->setup("assets/border.png", getRenderer(), getWidth() / 2.0, getHeight() - 8, getWidth(), 16);
+		addGameObject(bottomBorder);
+		rightBorder = new Border();
+		rightBorder->setup("assets/border.png", getRenderer(), getWidth() - 8, getHeight() / 2.0, 16, getHeight());
+		addGameObject(rightBorder);
 	}
 
-	void beforeUpdate()
+	void afterSetup()
 	{
-		printf("MyGameEngine::beforeUpdate\n");
+		setupBorders();
+
+		puck = new Puck();
+		puck->setup("assets/puck.png", getRenderer(), getWidth() / 2.0, getHeight() / 2.0, 64, 64);
+		puck->setVelocityX(1000.0);
+		puck->setVelocityY(1000.0);
+		addGameObject(puck);
 	}
 
 	void afterUpdate()
 	{
-		printf("MyGameEngine::afterUpdate\n");
+		if (puck->collidesWith(bottomBorder))
+		{
+			puck->setY(bottomBorder->getY() - (bottomBorder->getHeight() / 2.0) - (puck->getHeight() / 2.0) - 1);
+			puck->setVelocityY(puck->getVelocityY() * -1);
+		}
+		if(puck->collidesWith(topBorder))
+		{
+			puck->setY(topBorder->getY() + (topBorder->getHeight() / 2.0) + (puck->getHeight() / 2.0) + 1);
+			puck->setVelocityY(puck->getVelocityY() * -1);
+		}
+		if (puck->collidesWith(leftBorder))
+		{
+			puck->setX(leftBorder->getX() + (leftBorder->getWidth() / 2.0) + (puck->getWidth() / 2.0) + 1);
+			puck->setVelocityX(puck->getVelocityX() * -1);
+		}
+		if (puck->collidesWith(rightBorder))
+		{
+			puck->setX(rightBorder->getX() - (rightBorder->getWidth() / 2.0) - (puck->getWidth() / 2.0) - 1);
+			puck->setVelocityX(puck->getVelocityX() * -1);
+		}
 	}
-
-	void beforeRender()
-	{
-		printf("MyGameEngine::beforeRender\n");
-	}
-
-	void afterRender()
-	{
-		printf("MyGameEngine::afterRender\n");
-	}
-
-	void beforeTeardown()
-	{
-		printf("MyGameEngine::beforeTeardown\n");
-	}
-
 };
 
 int main()
 {
 	MyGameEngine *engine = new MyGameEngine();
 
-	if (engine->setup("My Example Game", 640, 480, false, 30) == 0)
+	if (engine->setup("My Example Game", 1024, 768, false, 60) == 0)
 	{
 		engine->run();
 		engine->teardown();
